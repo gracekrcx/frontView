143919126e628ac4507428c185bb30dfe6e2015f
krcx <krcx@krcxteki-iMac.local>
2020-05-16 19:58:04 +0800

    loading

--- components/imgCardWrap/imgCard.js
+++ components/imgCardWrap/imgCard.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Avatar from '@material-ui/core/Avatar';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles((theme)=>({
   card: {
@@ -59,6 +60,7 @@ const useStyles = makeStyles((theme)=>({
     fontWeight: 200,
     'margin-left': 4,
     color: '#dcdcdc',
+    width: 100,
   },
   ctx:{
     display: 'box',
@@ -82,34 +84,69 @@ const useStyles = makeStyles((theme)=>({
   }
 }));
 
-export default function ImgCard() {
+export default function ImgCard(props) {
+  const { loading = false } = props;
   const classes = useStyles();
 
   return (
     <Card className={classes.card} elevation={0}>
       <div className={classes.mediaBox}>
-        <CardMedia
-          className={classes.media}
-          component="img"
-          title="Ted talk"
-          src='/static/placeholder.png'
-        />
+        {loading ? (
+          <Skeleton animation="wave" variant="rect" height={180}/>
+        ) : (
+          <CardMedia
+            className={classes.media}
+            component="img"
+            title="Ted talk"
+            src='/static/placeholder.png'
+          />
+        )}
       </div>
-      <div className={classes.title}>影評∣《美好拾光公司》人生充滿了投射、期望、自己寫的腳本</div>
+      {
+        loading ? (
+          <React.Fragment>
+            <Skeleton animation="wave" height={28}  />
+            <Skeleton animation="wave" height={28} style={{ marginBottom: 5}} />
+          </React.Fragment>
+        ) : (
+          <div className={classes.title}>影評∣《美好拾光公司》人生充滿了投射、期望、自己寫的腳本</div>
+        )
+      }
       <div className={classes.infoWrp}>
         <div className={classes.leftInfo}>
-          <Avatar
-            alt="Ted talk"
-            src="https://img.ezding.com.tw/photos/mymovie/act/ezding.png"
-          />
+          {
+            loading ? (
+              <Skeleton animation="wave" variant="circle" width={40} height={40} />
+            ) : (
+              <Avatar
+                alt="Ted talk"
+                src="https://img.ezding.com.tw/photos/mymovie/act/ezding.png"
+              />
+            )
+          }
           <div className={classes.day}>
-            <span>白色豆腐蛋糕</span><br/>
-            <span>2020/05/11</span>
+            {
+              loading ? (
+                <React.Fragment>
+                  <Skeleton animation="wave" height={20} width='50%'/>
+                  <Skeleton animation="wave" height={20} width='50%'/>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <div>白色豆腐蛋糕</div>
+                  <div>2020/05/11</div>
+                </React.Fragment>
+              )
+            }
           </div>
         </div>
         <div className={classes.icon}>有雷</div>
       </div>
-      <div className={classes.ctx}>2019 金馬獎最佳男主角、新導演、原創電影歌曲入圍｜2020 香港電影金像獎最佳男主角、女主角、女配角、新晉導演等八項大獎入圍｜榮獲迷影精神賞年度榮譽大奬、華</div>
+      {loading ? (
+        <Skeleton animation="wave" variant="rect" height={60} style={{ marginTop: 10}}/>
+      ) : (
+        <div className={classes.ctx}>2019 金馬獎最佳男主角、新導演、原創電影歌曲入圍｜2020 香港電影金像獎最佳男主角、女主角、女配角、新晉導演等八項大獎入圍｜榮獲迷影精神賞年度榮譽大奬、華</div>
+      )}
     </Card>
   );
 }
--- styled/materialUITheme.js
+++ styled/materialUITheme.js
@@ -26,6 +26,7 @@ const theme = createMuiTheme({
     ].join(','),
   },
   palette: {
+    type: 'dark',
     background:{
       default: '#404040'
     }
